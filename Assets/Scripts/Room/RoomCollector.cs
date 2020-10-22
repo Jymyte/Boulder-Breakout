1ab2250f5329080951fe86fa6b744e863eaa6639
Jymyte <jyrimy@gmail.com>
2020-10-22 14:00:24 +0300

    Tinkering with Room execution. Added running and jumping animations.

---
+++ Assets/Scripts/Room/RoomCollector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCollector : MonoBehaviour
+{
+    void OnTriggerEnter(Collider target) {
+        if (target.tag == "Room End") {
+            target.transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Room/RoomSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Rooms;
+    private List<GameObject> roomsToSpawn = new List<GameObject>();
+
+    void Awake()
+    {
+        initializeRooms();
+    }
+    void initializeRooms() {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
