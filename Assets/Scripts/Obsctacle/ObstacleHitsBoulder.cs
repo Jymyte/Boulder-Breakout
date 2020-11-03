e4814bf0280b17457d41a6f67e2a840b6b7bbc34
Jymyte <jyrimy@gmail.com>
2020-11-03 12:21:33 +0200

    Blocks deactivate on collision with boulder.
    Demo scene with grid for better room testing.

---
+++ Assets/Scripts/Obsctacle/ObstacleHitsBoulder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHitsBoulder : MonoBehaviour {
+
+	void OnTriggerEnter(Collider target) {
+		if (target.tag == "Boulder") {
+			gameObject.SetActive(false);
+		}
+	}
+
+}
