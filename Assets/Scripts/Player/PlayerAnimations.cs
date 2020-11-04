67fd0e6886b841456280ac137f3071854a1d225b
Jymyte <jyrimy@gmail.com>
2020-11-04 14:07:08 +0200

    Progressing on the animation transitions

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rigidbody.velocity.y > 0)
+            anim.Play("Jumping");
+    }
+}
