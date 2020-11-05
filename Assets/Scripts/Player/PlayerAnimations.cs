0eb20b86b076e74e68b4a7b1d655b4a8691dffa1
Jymyte <jyrimy@gmail.com>
2020-11-05 10:11:36 +0200

    Replaced old Gameplay scene with Spazi's version.

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
+    private GameObject player;
     private Rigidbody _rigidbody;
     private Animator anim;
 
+    private float previousYVelocity; 
+    private float currentYVelocity;
+
+    private void Awake() {
+        player = GameObject.Find("Player");
+        _rigidbody = player.GetComponent<Rigidbody>();
+        anim = GetComponent<Animator> ();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (_rigidbody.velocity.y > 0)
+        currentYVelocity = _rigidbody.velocity.y;
+
+        if (currentYVelocity > 0 || previousYVelocity <= 0)
             anim.Play("Jumping");
     }
 }
