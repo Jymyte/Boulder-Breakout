a4cfe8385c32aae5cb3a84d3d5bb8cdd719ced72
Jymyte <jyrimy@gmail.com>
2020-11-05 11:06:49 +0200

    Animations work!

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -15,13 +15,32 @@ public class PlayerAnimations : MonoBehaviour
         player = GameObject.Find("Player");
         _rigidbody = player.GetComponent<Rigidbody>();
         anim = GetComponent<Animator> ();
+        //anim.Play("JumpTransition");
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         currentYVelocity = _rigidbody.velocity.y;
 
-        if (currentYVelocity > 0 || previousYVelocity <= 0)
-            anim.Play("Jumping");
+        if (currentYVelocity > 0 && previousYVelocity <= 0) {
+            anim.Play("JumpStart");
+            Debug.Log(currentYVelocity + "current" + previousYVelocity + "previous");
+        }
+        if (currentYVelocity <= 0 && previousYVelocity > 0) {
+            anim.Play("JumpTransition");
+            Debug.Log("PLAYING JUMP TRANSITION");
+        }
+        if (currentYVelocity < 0 && previousYVelocity < 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("JumpTransition")) {
+            anim.Play("Falling");
+             Debug.Log("PLAYING FALLING");
+        }
+        if (currentYVelocity == 0 && previousYVelocity == 0) {
+            anim.Play("Run");
+        }
+
+        Debug.Log(currentYVelocity + "Current" + previousYVelocity + "previous");
+
+        previousYVelocity = currentYVelocity;
+    
     }
 }
